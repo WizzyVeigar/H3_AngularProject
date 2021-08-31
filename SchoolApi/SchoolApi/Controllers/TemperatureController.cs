5459e2aeafb2585d2a79e41a9ec248b45fd3b8e8
benjaminbhr <56117081+benjaminbhr@users.noreply.github.com>
2021-08-31 10:18:43 +0200

    Created SchoolAPi

---
+++ SchoolApi/SchoolApi/Controllers/TemperatureController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SchoolApi.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class TemperatureController : ControllerBase
+    {
+
+        [HttpPost]
+        public string PostTemperature(string temp)
+        {
+            return temp;
+        }
+    }
+}
