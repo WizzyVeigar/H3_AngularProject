2d928641c7a4b38caa412d68c169772b52a2e12d
benjaminbhr <56117081+benjaminbhr@users.noreply.github.com>
2021-09-28 14:37:54 +0200

    Angular login changes, and API Cors change

--- SchoolApi/SchoolApi/Startup.cs
+++ SchoolApi/SchoolApi/Startup.cs
@@ -32,7 +32,7 @@ namespace SchoolApi
             services.AddDbContext<SchoolContext>();
             services.AddScoped<DbContext, SchoolContext>();
             services.AddCors(options => options.AddDefaultPolicy(
-                builder => builder.AllowAnyOrigin()));
+                builder => builder.AllowAnyOrigin().AllowAnyHeader()));
 
             services.AddSwaggerGen(c =>
             {
